43572fe1bb17f8a039cf0eccc1752c5989e52dbf
ashraddhansh <ashraddhansh@protonmail.com>
2026-06-17 21:03:08 +0530

    implemented remove_stopwords function

---
+++ cli/constants.py
@@ -0,0 +1,2 @@
+JSON_FILE = "/home/ashraddhansh/Developer/github.com/rag-search-engine/data/movies.json"
+STOPWORDS_FILES = "/home/ashraddhansh/Developer/github.com/rag-search-engine/data/stopwords.txt"
--- cli/keyword_search.py
+++ cli/keyword_search.py
@@ -1,6 +1,11 @@
 import json
+from os import remove
 import string
-JSON_FILE = "/home/ashraddhansh/Developer/github.com/rag-search-engine/data/movies.json"
+from constants import JSON_FILE, STOPWORDS_FILES
+
+PROC_STOPWORDS = []
+
+
 
 def tokenize(query) -> list:
     return list(filter(None, query.split()))
@@ -9,8 +14,18 @@ def remove_punctuation(query):
     punc_to_empty_mapping = str.maketrans({item: "" for item in string.punctuation})
     return query.translate(punc_to_empty_mapping)
 
-def pre_process(query):
-    return tokenize(remove_punctuation(query.lower()))
+
+with open(STOPWORDS_FILES, 'r') as f:
+    PROC_STOPWORDS = {
+            remove_punctuation(x).lower()
+            for x in f.read().splitlines()
+            }
+
+def remove_stopwords(token_list):
+        return [x for x in token_list if x not in PROC_STOPWORDS]
+
+def pre_process(query) -> list:
+    return remove_stopwords(tokenize(remove_punctuation(query.lower())))
 
 def keyword_search(query):
     with open(JSON_FILE, 'r') as f:
